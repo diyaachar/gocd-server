afb69263e1cf21f1bc68d157b22126dee6dc12a5
diyaachar <divya_dia@live.com>
2018-05-29 09:32:38 -0700

    agent restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 /etc/init.d/go-server start 
 /etc/init.d/go-agent start
 sed -i -e 's/127.0.0.1/goserver/g' /etc/default/go-agent
+/etc/init.d/go-agent restart
 tail -f /var/log/go-server/go-server.log
