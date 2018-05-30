cb3a5e57be6360fa7aca7e2c1a8bf5ec8d381855
Divya <divya_dia@live.com>
2018-05-30 09:34:24 +0530

    go-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #! bin/bash
 /etc/init.d/go-server start 
-/etc/init.d/go-agent start
-sed -i -e 's/127.0.0.1/goserver/g' /etc/default/go-agent
-/etc/init.d/go-agent restart
 tail -f /var/log/go-server/go-server.log
